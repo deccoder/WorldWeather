49aacc542b8086b38046a6011d524275abd4e0ff
deccoder <declanbrophy@gmail.com>
2019-06-16 22:22:02 +0100

    User Interface Design

--- app/src/main/java/declanbrophy/worldweather/MainActivity.java
+++ app/src/main/java/declanbrophy/worldweather/MainActivity.java
@@ -1,13 +1,46 @@
 package declanbrophy.worldweather;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import java.io.*;
+import java.net.*;
+import java.net.URLConnection;
 
-public class MainActivity extends AppCompatActivity {
+
+
+
+public class MainActivity extends AppCompatActivity  {
+
+
+    public static final String API_Key = "ae3723984918e29156906ffa2182bf02";
+
+    public static final String Website_Id = "http://openweathermap.org/forecast16";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        //Initialize button on user interface
+        Button submit = (Button) findViewById(R.id.Submit);
+
+        //Give button functionality
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+
+
+
+
+        }
+
+
 }
